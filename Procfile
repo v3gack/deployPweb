400ee3512a3828c2fd226230a803fe72afcf8a04
v3gack <elvis.vega24724@gmail.com>
2025-06-26 19:56:44 -0400

    deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: npm --prefix programacion-web-back start
--- programacion-web-back/src/index.ts
+++ programacion-web-back/src/index.ts
@@ -6,10 +6,15 @@ import preguntasRoutes from './routes/pregunta.route';
 import usuarioRoutes from './routes/usuario.route';
 import dotenv from 'dotenv';
 
+import path from 'path';
+
 dotenv.config(); // Para poder usar variables de entorno (como SECRET)
 
 const app = express();
 const prisma = new PrismaClient();
+
+//dep
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors({
@@ -42,6 +47,15 @@ app.use('/api/pregunta', preguntasRoutes);
 app.use('/api/usuario', usuarioRoutes);
 
 // Inicio del servidor
-app.listen(3001, () => {
-  console.log('Servidor corriendo en http://localhost:3001');
+// app.listen(3001, () => {
+//   console.log('Servidor corriendo en http://localhost:3001');
+// });
+app.use(express.static(path.join(__dirname, '../../programacion-web/build')));
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../../programacion-web/build/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Servidor escuchando en puerto ${PORT}`);
 });
